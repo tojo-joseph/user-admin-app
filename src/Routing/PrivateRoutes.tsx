b39896e2011b175eb5ad4e74142e87a33e500162
tojo-joseph <tojo.joseph18@gmail.com>
2025-01-08 23:40:31 +0530

    new login changes

--- src/Routing/PrivateRoutes.tsx
+++ src/Routing/PrivateRoutes.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../auth/login/login";
 
 export default function PrivateRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
